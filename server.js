81e38a7736df39d9bb08a7a17c03d89914da57a3
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-03-28 13:31:03 +0300

    init: first commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ io.on("connection", function (socket) {
   });
 });
 
-const port = process.env.PORT | process.env.PORT | 3000;
+const port = process.env.PORT || process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
